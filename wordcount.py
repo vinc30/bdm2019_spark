7a59386e85615c785be1978d7a014e386628ba9d
YuJhengFang <solarorb@gmail.com>
2019-03-26 19:22:04 -0400

    wordcount example

---
+++ wordcount.py
@@ -0,0 +1,10 @@
+from pyspark import SparkContext
+
+if __name__ == "__main__":
+    sc = SparkContext()
+    sc.textFile('book.txt')\
+    .flatMap(lambda x: x.split())\
+    .map(lambda x: (x,1))\
+    .reduceByKey(lambda x, y: x + y)\
+    .saveAsTestFile('output')
+
