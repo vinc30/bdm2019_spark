48fbcc978b2bbd45dabbe177b2b4f40d56d54cbb
YuJhengFang <solarorb@gmail.com>
2019-03-26 19:45:02 -0400

    lab6 code

---
+++ lab6.py
@@ -0,0 +1,51 @@
+# It's usually better to factor things out for readability
+from pyspark import SparkContext
+
+def createIndex(shapefile):
+    import rtree
+    import fiona.crs
+    import geopandas as gpd
+    zones = gpd.read_file(shapefile).to_crs(fiona.crs.from_epsg(2263))
+    index = rtree.Rtree()
+    for idx,geometry in enumerate(zones.geometry):
+        index.insert(idx, geometry.bounds)
+    return (index, zones)
+
+def findZone(p, index, zones):
+    match = index.intersection((p.x, p.y, p.x, p.y))
+    for idx in match:
+        if zones.geometry[idx].contains(p):
+            return idx
+    return None
+
+def processTrips(pid, records):
+    import csv
+    import pyproj
+    import shapely.geometry as geom
+    
+    proj = pyproj.Proj(init="epsg:2263", preserve_units=True)    
+    index, zones = createIndex('ZillowNeighborhoods-NY/ZillowNeighborhoods-NY.shp')    
+    
+    if pid==0:
+        next(records)
+    reader = csv.reader(records)
+    counts = {}
+    
+    for row in reader:
+        pdt = row[0].split(' ')[1].split(':')[0]
+        if pdt!='10':
+            continue
+        p = geom.Point(proj(float(row[3]), float(row[2])))
+        zone = findZone(p, index, zones)
+        if zone:
+            counts[zone] = counts.get(zone, 0) + 1
+    return counts.items()
+ 
+if __name__ == "__main__":           
+    sc = SparkContext()
+    rdd = sc.textFile('/tmp/bdm/green.csv')
+    counts = rdd.mapPartitionsWithIndex(processTrips) \
+		.reduceByKey(lambda x,y: x+y) \
+		.collect()
+    print(counts)
+
